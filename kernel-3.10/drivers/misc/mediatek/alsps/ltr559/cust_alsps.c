00bd12e40d536a4f40c106449bb8fddde925368f
Smosia <0stas0@mail.ru>
2018-02-05 23:27:29 +0300

    some alps stuff (trying to fix proximity sensor)

--- kernel-3.10/drivers/misc/mediatek/alsps/ltr559/cust_alsps.c
+++ kernel-3.10/drivers/misc/mediatek/alsps/ltr559/cust_alsps.c
@@ -2,21 +2,20 @@
 #include <mach/mt_pm_ldo.h>
 #include <cust_alsps.h>
 
-static struct alsps_hw cust_alsps_hw = {
+static struct alsps_hw ltr559_cust_alsps_hw = {
     .i2c_num    = 3,
     .polling_mode_ps =0,
     .polling_mode_als =1,
     .power_id   = MT65XX_POWER_NONE,    /*LDO is not used*/
     .power_vol  = VOL_DEFAULT,          /*LDO is not used*/
     .i2c_addr   = {0x72, 0x48, 0x78, 0x00},
-    .als_level  = {0, 150, 328, 861, 1377, 3125, 7721, 7767, 12621, 23062, 28430, 33274, 47116, 57694, 57694, 65535, 65535},
-    .als_value  = {0, 50, 133, 304, 502, 1004, 2005, 3058, 5005, 8008, 10010, 12000, 16000, 20000, 20000, 20000, 20000, 24000},
-    // .als_level  = {1, 4, 10, 30, 120, 150, 255, 380, 650, 900, 1350, 2200, 3500, 6000, 10000, 20000, 50000},
-    // .als_value  = {0, 5, 10, 30, 80, 120, 225, 320, 550, 800, 1250, 2000, 3000, 5000, 5500, 7500, 13000, 24000},
-    .ps_threshold_high = 1050,
-    .ps_threshold_low = 980,
+    .als_level  = {1, 4, 10, 30, 120, 150, 255, 380, 650, 900, 1350, 2200, 3500, 6000, 10000, 20000, 50000},
+    .als_value  = {0, 5, 10, 30, 80, 120, 225, 320, 550, 800, 1250, 2000, 3000, 5000, 5500, 7500, 13000, 24000},
+    .ps_threshold_high = 1000,
+    .ps_threshold_low = 800,
 };
-struct alsps_hw *get_cust_alsps_hw(void) {
-    return &cust_alsps_hw;
+
+struct alsps_hw *ltr559_get_cust_alsps_hw(void) {
+    return &ltr559_cust_alsps_hw;
 }
 
